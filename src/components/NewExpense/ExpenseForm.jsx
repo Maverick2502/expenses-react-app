6b16cedfe94ab98f80bc65453a87293aeda4ca7b
Nurullo <khuwaidullaev@gmail.com>
2021-05-14 01:30:54 +0500

    Handling Form Submission

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -18,8 +18,20 @@ function ExpenseForm() {
     setEnteredDate(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: enteredDate,
+    };
+
+    console.log(expenseData);
+  }
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
@@ -27,11 +39,21 @@ function ExpenseForm() {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2021-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2021-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
